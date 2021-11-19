91059c71b96d557a063de2897d3e69ffeb3794dd
Aaron Basco <aron42610@hotmail.com>
2021-11-19 18:42:13 +0800

    ADDED and ENABLED vue

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -37,7 +37,7 @@
     <!-- Brand Logo -->
     <a href="../../index3.html" class="brand-link">
       <img src="../../dist/img/AdminLTELogo.png" alt="Simple Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">SIMPLE</span>
+      <span class="brand-text font-weight-light">SiMPLE</span>
     </a>
 
     <!-- Sidebar -->
