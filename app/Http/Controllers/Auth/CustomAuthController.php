5e199409c918e1e8b50207d4a2afa5bd2a4b851b
Aaron Basco <aron42610@hotmail.com>
2021-11-20 13:15:08 +0800

    ADDED sanctum and custom auth controller, also added Carbon

---
+++ app/Http/Controllers/Auth/CustomAuthController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+use Carbon\Carbon;
+
+class CustomAuthController extends Controller
+{
+    /**
+     * User registration
+     */
+    public function register(Request $request){
+        $data = $request->validate([
+            'username' => 'required|string|unique:users,username|max:255',
+            'email' => 'required|string|unique:users,email|max:255',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'username'  => $data['username'],
+            'email'     => $data['email'],
+            'password'  => Hash::make($data['password']),
+        ]);
+
+        $tokenString = Hash::make($data['username'] . "|" . Carbon::now()->format('YmdHis'));
+        $token = $user->createToken($tokenString)->plainTextToken;
+
+        $response = [
+            'user'  => $user,
+            'token' => $token,
+        ];
+
+        return response($response, 201);
+    }
+
+    /**
+     * User logout
+     */
+    public function logout(Request $request){
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'Logged out',
+        ];
+    }
+
+    /**
+     * User login
+     */
+    public function login(Request $request){
+        $data = $request->validate([
+            'username' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('username', $data['username'])->first();
+        if(!$user){
+            $user = User::where('email', $data['email'])->first();
+        }
+
+        if( !$user || !Hash::check($data['password'], $user->password) ) {
+            return response([
+                'message' => 'Login failed. Incorrect username or password'
+            ], 401);
+        }
+
+        $tokenString = Hash::make($data['username'] . "|" . Carbon::now()->format('YmdHis'));
+        $token = $user->createToken($tokenString)->plainTextToken;
+
+        $response = [
+            'user'  => $user->username,
+            'token' => $token,
+        ];
+
+        return response($response, 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Product;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Auth\CustomAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,16 +21,33 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-# Product Resource
-Route::resource('products', ProductController::class);
+/**
+ * Exposed Routes
+ * */
 
+# GET products
+Route::get('/products', [ProductController::class, 'index']);
+
+# GET product using id
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
+# GET product search
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
-# POST create product
-// Route::post('/products', [ProductController::class, 'store']);
+# POST user register
+Route::post('/register', [CustomAuthController::class, 'register']);
 
-// # GET product
-// Route::post('/products/$1', [ProductController::class, 'show']);
+# POST user login
+Route::post('/login', [CustomAuthController::class, 'login']);
 
-// # GET products
-// Route::get('/products', [ProductController::class, 'index']);
+/**
+ * Protected Routes
+ * */
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+    Route::post('/logout', [CustomAuthController::class, 'logout']);
+});
+
