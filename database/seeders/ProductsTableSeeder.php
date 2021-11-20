db75d87dc2c259df9b1a2449b67c2d8caa402438
Aaron Basco <aron42610@hotmail.com>
2021-11-20 14:43:10 +0800

    ADDED product seeder

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -2,17 +2,19 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
+
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ProductsTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        \App\Models\Product::factory(10)->create();
+        factory(Product::class, 30)->create();
     }
 }
